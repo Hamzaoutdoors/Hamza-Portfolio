a67b89b2e7bfb67ed0fbbd7fbc628f4be06679e5
Hamza <hamzaoutdoors@gmail.com>
2022-02-15 01:43:20 +0100

    feat: Add Testimonials section

---
+++ src/components/testimonials/Testimonials.js
@@ -0,0 +1,32 @@
+import './testimonials.scss';
+import testimonialsData from '../../utils/testimonialsData';
+
+const Testimonials = () => (
+  <div className="testimonials" id="testimonials">
+    <h1>Testimonials</h1>
+    <div className="container">
+      {testimonialsData.map((test) => (
+        <div className={test.featured ? 'card featured' : 'card'} key={test.id}>
+          <div className="top">
+            <img src="assets/right-arrow.png" className="left" alt="" />
+            <img
+              className="user"
+              src={test.img}
+              alt=""
+            />
+            <img className="right" src={test.icon} alt="" />
+          </div>
+          <div className="center">
+            {test.desc}
+          </div>
+          <div className="bottom">
+            <h3>{test.name}</h3>
+            <h4>{test.title}</h4>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+export default Testimonials;
