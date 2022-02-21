6a956bf6dfebcf1a6172465bc6948f37b8cfd652
Hamza <hamzaoutdoors@gmail.com>
2022-02-21 19:10:00 +0100

    feat: Add Hero section with styling using SASS

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -2,6 +2,17 @@
 import './intro.scss';
 import { init } from 'ityped';
 import { useEffect, useRef } from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import Social from './Social';
+import { phone } from '../../responsive';
+
+const ResumeButton = styled.a`
+  display: block;
+  ${phone({
+    display: 'none',
+  })};
+`;
 
 export default function Intro() {
   const textRef = useRef();
@@ -11,13 +22,28 @@ export default function Intro() {
       showCursor: true,
       backDelay: 1500,
       backSpeed: 60,
-      strings: ['Full-Stack Web Developer', 'Geoinformatics Enthusiast', 'Software Engineer'],
+      strings: ['Full-Stack Web Developer', 'ReactJS Enthusiast', 'GIS Engineer'],
     });
   }, []);
 
   return (
     <div className="intro" id="intro">
-      <div className="right">
+      <motion.div
+        className="right"
+        initial={{
+          opacity: 0,
+          translateX: -70,
+        }}
+        animate={{
+          opacity: 1.2,
+          translateX: 0,
+        }}
+        transition={{
+          duration: 0.4,
+          delay: 0.2,
+        }}
+      >
+        <Social />
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
           <h1>El Laouzi Hamza</h1>
@@ -26,16 +52,33 @@ export default function Intro() {
             {' '}
             <span ref={textRef} />
           </h3>
+          <ResumeButton href="https://drive.google.com/file/d/1ZP4CqrDBnGgCJMPAcoTD-GJiGErRMFqD/view" className="btn" target="_blank" rel="noreferrer">
+            Get My Resume
+          </ResumeButton>
         </div>
-        <a href="#portfolio">
+        <a href="#portfolio" className="down">
           <img src="assets/down.png" alt="" />
         </a>
-      </div>
-      <div className="left">
+      </motion.div>
+      <motion.div
+        className="left"
+        initial={{
+          opacity: 0,
+          translateX: 70,
+        }}
+        animate={{
+          opacity: 1.2,
+          translateX: -5,
+        }}
+        transition={{
+          duration: 0.6,
+          delay: 0.3,
+        }}
+      >
         <div className="imgContainer">
           <img src="assets/images/me2.png" alt="" />
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
