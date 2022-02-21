cc965c31421207b879f371d866fc0c59078b2d59
Hamza <hamzaoutdoors@gmail.com>
2022-02-21 19:05:14 +0100

    feat: Add footer component in App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Works from './components/works/Works';
 import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
 import Menu from './components/menu/Menu';
+import Footer from './components/footer/Footer';
+
 import './app.scss';
 
 const App = () => {
@@ -20,6 +22,7 @@ const App = () => {
         <Works />
         <Testimonials />
         <Contact />
+        <Footer />
       </div>
     </div>
   );
