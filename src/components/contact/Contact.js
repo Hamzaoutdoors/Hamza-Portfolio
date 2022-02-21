992df6ac52c8660c1f865468c04002826ebe8d3b
Hamza <hamzaoutdoors@gmail.com>
2022-02-21 19:07:41 +0100

    feat: Add Contact section with styling using SASS

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,27 +1,140 @@
-import { useState } from 'react';
+/* eslint-disable no-nested-ternary */
+/* eslint-disable implicit-arrow-linebreak */
 import './contact.scss';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
 
-export default function Contact() {
-  const [message, setMessage] = useState(false);
+const ContactWrapper = styled.div``;
+const Left = styled.div``;
+const Right = styled.div``;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setMessage(true);
+const Error = styled.div`
+  color: red;
+  font-size: 1.2rem;
+  font-weight: 500;
+  `;
+
+const Contact = () => {
+  const [status, setStatus] = useState({
+    submitted: false,
+    submitting: false,
+    info: { error: false, msg: null },
+  });
+
+  const [inputs, setInputs] = useState({
+    email: '',
+    message: '',
+  });
+
+  const handleServerResponse = (ok, msg) => {
+    if (ok) {
+      setStatus({
+        submitted: true,
+        submitting: false,
+        info: { error: false, msg },
+      });
+      setInputs({
+        email: '',
+        message: '',
+      });
+    } else {
+      setStatus({
+        info: { error: true, msg },
+      });
+    }
   };
+  const handleOnChange = (e) => {
+    e.persist();
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+    setStatus({
+      submitted: false,
+      submitting: false,
+      info: { error: false, msg: null },
+    });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
+    axios({
+      method: 'POST',
+      url: 'https://formspree.io/f/mayadege',
+      data: inputs,
+    })
+      // eslint-disable-next-line no-unused-vars
+      .then((response) => {
+        handleServerResponse(
+          true,
+          'Thank you, your message has been submitted.',
+        );
+      })
+      .catch((error) => {
+        handleServerResponse(false, error.response.data.error);
+      });
+  };
+
   return (
-    <div className="contact" id="contact">
-      <div className="left">
+    <ContactWrapper className="contact" id="contact">
+      <Left className="left">
         <img src="assets/images/contactme1.svg" alt="" />
-      </div>
-      <div className="right">
-        <h2>Contact.</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" className="form-control" />
-          <textarea placeholder="Message" className="form-control" row="3" />
-          <button type="submit">Send</button>
-          {message && <span>Thanks, I`ll reply ASAP</span>}
+      </Left>
+      <Right className="right">
+        <h2>CONTACT.</h2>
+        <h3>
+          I am always open to new opportunities and would love to hear from you.
+        </h3>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            id="name"
+            type="text"
+            placeholder="Full Name"
+            name="name"
+            onChange={handleOnChange}
+            className="form-control"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="form-control"
+            id="email"
+            onChange={handleOnChange}
+            required
+          />
+          <textarea
+            placeholder="Enter your message"
+            className="form-control"
+            name="message"
+            row="3"
+            maxLength="500"
+            id="message"
+            onChange={handleOnChange}
+            required
+          />
+          <button type="submit" disabled={status.submitting}>
+            {!status.submitting
+              ? !status.submitted
+                ? 'Get In Touch'
+                : 'Submitted'
+              : 'Submitting...'}
+          </button>
+          {status.info.error && (
+          <Error>
+            Error:
+            {' '}
+            {status.info.msg}
+          </Error>
+          )}
+          {!status.info.error && status.info.msg && <p>{status.info.msg}</p>}
         </form>
-      </div>
-    </div>
+      </Right>
+    </ContactWrapper>
   );
-}
+};
+
+export default Contact;
